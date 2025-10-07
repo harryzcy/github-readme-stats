69cc423af964c375cb2bfab62a7e4beb00de8339
Ophelia Goldstein <159258143+opheliagoldstein@users.noreply.github.com>
2025-10-08 02:12:28 +0300

    feat: disable caching when NODE_ENV is development (#4543)
    Co-authored-by: Alexandr <qwerty541zxc@gmail.com>

--- src/common/cache.js
+++ src/common/cache.js
@@ -48,7 +48,7 @@ const disableCaching = (res) => {
  * @param {number} cacheSeconds The cache seconds to set in the headers.
  */
 const setCacheHeaders = (res, cacheSeconds) => {
-  if (cacheSeconds < 1) {
+  if (cacheSeconds < 1 || process.env.NODE_ENV === "development") {
     disableCaching(res);
     return;
   }
@@ -70,7 +70,10 @@ const setErrorCacheHeaders = (res) => {
   const envCacheSeconds = process.env.CACHE_SECONDS
     ? parseInt(process.env.CACHE_SECONDS, 10)
     : NaN;
-  if (!isNaN(envCacheSeconds) && envCacheSeconds < 1) {
+  if (
+    (!isNaN(envCacheSeconds) && envCacheSeconds < 1) ||
+    process.env.NODE_ENV === "development"
+  ) {
     disableCaching(res);
     return;
   }
